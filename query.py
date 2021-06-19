c759b32be4117aa2ed44ee940bb876a42629b56e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-06-19 15:16:25 +0530

    updates

--- query.py
+++
@@ -1,5 +0,0 @@
-projection = {
-    "vendor_id_renamed": ds.field("VendorID"),
-    "total_amount_more_than_27": ds.field("total_amount") > 27,
-    "payment_type_2": ds.field("payment_type") == 2
-}
