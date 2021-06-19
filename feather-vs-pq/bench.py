9ed536803dc5aaa6244df16b719bcd2ea6d61025
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-06-19 19:30:55 +0530

    add  bench code

--- feather-vs-pq/bench.py
+++ feather-vs-pq/bench.py
@@ -1,43 +1,68 @@
 import pyarrow.feather as feather
 import pyarrow.parquet as pq
 import time
+import os
+
+def drop_caches():
+    os.system('sync')
+    os.system('sync')
+
+read_time = list()
+for i in range(50):
+    drop_caches()
+    s = time.time()
+    open('128MB.feather', 'rb').read()
+    e = time.time()
+    read_time.append(e-s)
+print("read feather blob:", sum(read_time)/len(read_time))
+
+
+read_time = list()
+for i in range(50):
+    drop_caches()
+    s = time.time()
+    open('128MB.parquet', 'rb').read()
+    e = time.time()
+    read_time.append(e-s)
+print("read pq blob:", sum(read_time)/len(read_time))
 
 ## parquet
 pq_time = list()
-for i in range(10):
+for i in range(50):
+    drop_caches()
     s = time.time()
     table = pq.read_table('128MB.parquet')
     e = time.time()
     pq_time.append(e-s)
-print(sum(pq_time)/len(pq_time))
+print("parquet all columns:", sum(pq_time)/len(pq_time))
 
 ## feather
 feather_time = list()
-for i in range(10):
+for i in range(50):
+    drop_caches()
     s = time.time()
-    with open('128MB.feather', 'rb') as f:
-        read_df = feather.read_table(f)
+    read_df = feather.read_table('128MB.feather', memory_map=True)
     e = time.time()
     feather_time.append(e-s)
-print(sum(feather_time)/len(feather_time))
-
-# col reads
+print("feather all columns: ", sum(feather_time)/len(feather_time))
 
 ## parquet
 pq_time = list()
-for i in range(10):
+for i in range(50):
+    drop_caches()
     s = time.time()
     table = pq.read_table('128MB.parquet', columns=['total_amount'])
     e = time.time()
     pq_time.append(e-s)
-print(sum(pq_time)/len(pq_time))
+print("parquet single column:", sum(pq_time)/len(pq_time))
 
 ## feather
 feather_time = list()
-for i in range(10):
+for i in range(50):
+    drop_caches()
     s = time.time()
-    with open('128MB.feather', 'rb') as f:
-        read_df = feather.read_table(f, columns=['total_amount'])
+    read_df = feather.read_table('128MB.feather', columns=['total_amount'], memory_map=True)
     e = time.time()
     feather_time.append(e-s)
-print(sum(feather_time)/len(feather_time))
+print("feather single column:", sum(feather_time)/len(feather_time))
+
