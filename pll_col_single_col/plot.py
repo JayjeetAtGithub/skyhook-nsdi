175898c88708c71a59af30e920fca311f36ff627
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-07-16 18:41:28 +0530

    fixes

--- pll_col_single_col/plot.py
+++ pll_col_single_col/plot.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     palette = sns.color_palette('Greys', n_colors=2)
     results = []
 
-    with open('pq.json') as json_file:
+    with open('pq_16_pll_col.json') as json_file:
         data_dict = json.load(json_file)
         print(data_dict)
 
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         for val in v:
             results.append([k, val, 'parquet-128MB'])
 
-    with open('rpq.json') as json_file:
+    with open('rpq_16_pll_col.json') as json_file:
         data_dict = json.load(json_file)
         print(data_dict)
 
@@ -34,5 +34,5 @@ if __name__ == "__main__":
     sns_plot = sns.barplot(x="Selectivity (%)", y="Duration (s)", hue='Format', data=df, ci='sd', capsize=.15, errwidth=0.5, palette=palette)
     sns_plot.set(ylim=(0, 150))
     plt.tight_layout()
-    plt.savefig(f"plot.pdf", bbox_inches='tight')
+    plt.savefig(f"plot16_col.pdf", bbox_inches='tight')
 
