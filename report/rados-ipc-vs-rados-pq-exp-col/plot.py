7c03e5e4af609ca38514ac24393fa3b4a1954bef
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-07-21 20:02:53 +0530

    updates

--- report/rados-ipc-vs-rados-pq-exp-col/plot.py
+++ report/rados-ipc-vs-rados-pq-exp-col/plot.py
@@ -10,37 +10,37 @@ if __name__ == "__main__":
     palette = sns.color_palette('Greys', n_colors=4)
     results = []
 
-    with open('vanilla-pq-128.json') as json_file:
+    with open('vanilla-pq-16.json') as json_file:
         data_dict = json.load(json_file)
         print(data_dict)
 
     for k, v in reversed(data_dict.items()):
         for val in v:
-            results.append([k, val, 'vanilla-parquet-128MB'])
+            results.append([k, val, 'vanilla-parquet-16MB'])
     
-    with open('rados-pq-128.json') as json_file:
+    with open('rados-pq-16.json') as json_file:
         data_dict = json.load(json_file)
         print(data_dict)
 
     for k, v in reversed(data_dict.items()):
         for val in v:
-            results.append([k, val, 'rados-parquet-128MB'])
+            results.append([k, val, 'rados-parquet-16MB'])
 
-    with open('vanilla-ipc-128.json') as json_file:
+    with open('vanilla-ipc-16.json') as json_file:
         data_dict = json.load(json_file)
         print(data_dict)
 
     for k, v in reversed(data_dict.items()):
         for val in v:
-            results.append([k, val, 'vanilla-ipc-128MB'])
+            results.append([k, val, 'vanilla-ipc-16MB'])
 
-    with open('rados-ipc-128.json') as json_file:
+    with open('rados-ipc-16.json') as json_file:
         data_dict = json.load(json_file)
         print(data_dict)
 
     for k, v in reversed(data_dict.items()):
         for val in v:
-            results.append([k, val, 'rados-ipc-128MB'])
+            results.append([k, val, 'rados-ipc-16MB'])
 
     df = pd.DataFrame(np.array(results), columns=['Selectivity (%)', 'Duration (s)', 'Format'])
     df[['Duration (s)']] = df[['Duration (s)']].apply(pd.to_numeric)
@@ -50,4 +50,4 @@ if __name__ == "__main__":
     sns_plot = sns.barplot(x="Selectivity (%)", y="Duration (s)", hue='Format', data=df, ci='sd', capsize=.15, errwidth=0.5, palette=palette)
     sns_plot.set(ylim=(0, 80))
     plt.tight_layout()
-    plt.savefig(f"rados-pq-vs-ipc-128MB.pdf", bbox_inches='tight')
+    plt.savefig(f"rados-pq-vs-ipc-16MB.pdf", bbox_inches='tight')
