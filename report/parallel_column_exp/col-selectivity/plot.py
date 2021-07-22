d7ad8085348345be8a29b0c8681ba0c0cd60f979
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-07-22 22:19:54 +0530

    updates

--- report/parallel_column_exp/col-selectivity/plot.py
+++ report/parallel_column_exp/col-selectivity/plot.py
@@ -7,18 +7,18 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    palette = sns.color_palette('Greys', n_colors=4)
+    palette = sns.color_palette('Greys', n_colors=2)
     results = []
 
-    with open('vanilla-pq-16.json') as json_file:
+    with open('pq_16_col_pll.json') as json_file:
         data_dict = json.load(json_file)
         print(data_dict)
 
     for k, v in reversed(data_dict.items()):
         for val in v:
             results.append([k, val, 'vanilla-parquet-16MB'])
-    
-    with open('rados-pq-16.json') as json_file:
+
+    with open('rpq_16_col_pll.json') as json_file:
         data_dict = json.load(json_file)
         print(data_dict)
 
@@ -26,21 +26,22 @@ if __name__ == "__main__":
         for val in v:
             results.append([k, val, 'rados-parquet-16MB'])
 
-    with open('vanilla-ipc-16.json') as json_file:
-        data_dict = json.load(json_file)
-        print(data_dict)
+    # with open('pq_128_col_pll.json') as json_file:
+    #     data_dict = json.load(json_file)
+    #     print(data_dict)
 
-    for k, v in reversed(data_dict.items()):
-        for val in v:
-            results.append([k, val, 'vanilla-ipc-16MB'])
+    # for k, v in reversed(data_dict.items()):
+    #     for val in v:
+    #         results.append([k, val, 'vanilla-parquet-128MB'])
+    
 
-    with open('rados-ipc-16.json') as json_file:
-        data_dict = json.load(json_file)
-        print(data_dict)
+    # with open('rpq_128_col_pll.json') as json_file:
+    #     data_dict = json.load(json_file)
+    #     print(data_dict)
 
-    for k, v in reversed(data_dict.items()):
-        for val in v:
-            results.append([k, val, 'rados-ipc-16MB'])
+    # for k, v in reversed(data_dict.items()):
+    #     for val in v:
+    #         results.append([k, val, 'rados-parquet-128MB'])
 
     df = pd.DataFrame(np.array(results), columns=['Selectivity (%)', 'Duration (s)', 'Format'])
     df[['Duration (s)']] = df[['Duration (s)']].apply(pd.to_numeric)
@@ -50,4 +51,4 @@ if __name__ == "__main__":
     sns_plot = sns.barplot(x="Selectivity (%)", y="Duration (s)", hue='Format', data=df, ci='sd', capsize=.15, errwidth=0.5, palette=palette)
     sns_plot.set(ylim=(0, 80))
     plt.tight_layout()
-    plt.savefig(f"rados-pq-vs-ipc-16MB.pdf", bbox_inches='tight')
+    plt.savefig(f"plot16_col.pdf", bbox_inches='tight')
